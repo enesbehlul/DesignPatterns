41ecf3d34ea183bc0d369e997f9b4ac8718a6146
unknown <ebehlul.yenidunya@stu.fsm.edu.tr>
2020-07-10 16:08:48 +0300

    singleton lazy loading ve double checked locking

--- src/Creationals/Singleton/Singleton.java
+++ src/Creationals/Singleton/Singleton.java
@@ -10,14 +10,22 @@ package Creationals.Singleton;
  * @author behlul
  */
 public class Singleton {
-    
-    private static Singleton singleton = new Singleton();
-    
-    private Singleton(){
+
+    private static Singleton singleton;
+
+    private Singleton() {
         System.out.println("Ben olustum");
     }
-    
-    public static Singleton getSingleton(){
+
+    public static Singleton getSingleton() {
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+
         return singleton;
     }
 }
